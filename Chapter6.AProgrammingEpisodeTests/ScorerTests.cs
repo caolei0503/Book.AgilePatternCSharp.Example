4c9f017f4bc84c4ffacfdba6056d05448e259558
caolei0503 <caolei_bjtu@163.com>
2016-09-03 12:19:00 +0800

    Add project files.

---
+++ Chapter6.AProgrammingEpisodeTests/ScorerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chapter6.AProgrammingEpisode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using NUnit.Framework;
+
+namespace Chapter6.AProgrammingEpisode.Tests
+{
+    [TestClass()]
+    public class ScorerTestS
+    {
+        [TestMethod()]
+        public void AddThrowTest()
+        {
+            Assert.AreEqual(1,1);
+        }
+
+        [TestMethod()]
+        public void ScoreForFrameTest()
+        {
+           // Assert.Fail();
+        }
+    }
+}
